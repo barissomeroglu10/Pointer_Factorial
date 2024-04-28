64f37a94fc7f536bcdb6cc63b8192d86631f24b0
baris <baris@BARIS_SOMEROGLU>
2024-04-28 15:51:16 +0300

    Add project files.

---
+++ Factorial/FactorialWithPointers.cpp
@@ -0,0 +1,41 @@
+﻿/*
+This code calculates factorial with pointers and functions.
+
+Developer = Barış Someroğlu
+Date = 28.04.2024 - 03:30 pm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Function prototype
+int RecursiveFactorial(int*);
+
+int main()
+{
+	int Number;
+
+	cout << "Pleas enter a number = ";
+	cin >> Number;
+
+	cout << "\nFactorail = " << RecursiveFactorial(&Number) << endl; // Call function
+
+	return 0;
+}
+
+// Function definition
+int RecursiveFactorial(int* ptrNumber)
+{
+	if (*ptrNumber < 0)
+		return -1;
+
+	else if (*ptrNumber <= 1)
+		return 1;
+
+	else {
+		int NextValue = *ptrNumber - 1; // Important line
+		return *ptrNumber * RecursiveFactorial(&NextValue);
+	}
+
+}
